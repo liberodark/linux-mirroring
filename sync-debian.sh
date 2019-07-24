13689c9d8da05459a55f3cc9fc1f28ce67e46056
liberodark <liberodark@gmail.com>
2019-07-24 13:31:49 +0200

    Update sync-debian.sh

--- sync-debian.sh
+++ sync-debian.sh
@@ -4,11 +4,7 @@ HOME="/tmp/http"
 TARGET="${HOME}/debian"
 TMP="${HOME}/.tmp/debian"
 LOCK="/tmp/rsync-debian.lock"
-ARCH_EXCLUDE="alpha arm armel armhf hppa hurd-i386 i386 ia64 kfreebsd-amd64 kfreebsd-i386 m68k mipsel mips powerpc s390 s390x sh sparc source"
-rsync_exclude=""
-for item in $ARCH_EXCLUDE; do
-    rsync_exclude="$rsync_exclude --exclude $item"
-done
+#EXCLUDE="${alpha arm armel armhf hppa hurd-i386 i386 ia64 kfreebsd-amd64 kfreebsd-i386 m68k mipsel mips powerpc s390 s390x sh sparc source}"
 
 # NOTE: You'll probably want to change this or remove the --bwlimit setting in
 # the rsync call below
@@ -26,7 +22,24 @@ if ! stty &>/dev/null; then
     QUIET="-q"
 fi
 
-rsync $rsync_exclude \
+rsync --exclude '*alpha*' \
+    --exclude '*arm*' \
+    --exclude '*armel*' \
+    --exclude '*armhf*' \
+    --exclude '*hppa*' \
+    --exclude '*hurd-i386*' \
+    --exclude '*i386*' \
+    --exclude '*ia64*' \
+    --exclude '*kfreebsd-amd64*' \
+    --exclude '*kfreebsd-i386*' \
+    --exclude '*m68k*' \
+    --exclude '*mipsel*' \
+    --exclude '*powerpc*' \
+    --exclude '*s390*' \
+    --exclude '*s390x*' \
+    --exclude '*sh*' \
+    --exclude '*sparc*' \
+    --exclude '*source*' \
     -rtlvH \
     --safe-links \
     --bwlimit=${BWLIMIT} \
