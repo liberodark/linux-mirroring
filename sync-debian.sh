d37fe5f164d5f2a481d12504fb77a460aa2ae55e
liberodark <liberodark@gmail.com>
2019-07-24 10:05:50 +0200

    Update sync-debian.sh

--- sync-debian.sh
+++ sync-debian.sh
@@ -4,6 +4,11 @@ HOME="/tmp/http"
 TARGET="${HOME}/debian"
 TMP="${HOME}/.tmp/debian"
 LOCK="/tmp/rsync-debian.lock"
+ARCH_EXCLUDE="alpha arm armel armhf hppa hurd-i386 i386 ia64 kfreebsd-amd64 kfreebsd-i386 m68k mipsel mips powerpc s390 s390x sh sparc source"
+rsync_exclude=""
+for item in $ARCH_EXCLUDE; do
+    rsync_exclude="$rsync_exclude --exclude $item"
+done
 
 # NOTE: You'll probably want to change this or remove the --bwlimit setting in
 # the rsync call below
@@ -21,11 +26,16 @@ if ! stty &>/dev/null; then
     QUIET="-q"
 fi
 
-rsync -rtlvH --safe-links \
+rsync $rsync_exclude \
+	-rtlvH \
+	--safe-links \
     --bwlimit=${BWLIMIT} \
     --delete-after --progress \
-    -h ${QUIET} --timeout=600 --contimeout=120 -p \
-    --delay-updates --no-motd \
+    -h ${QUIET} \
+    --timeout=600 \
+    --contimeout=120 -p \
+    --delay-updates \
+    --no-motd \
     --temp-dir="${TMP}" \
     ${SOURCE} \
     "${TARGET}"
