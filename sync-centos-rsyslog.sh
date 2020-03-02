b0de8ca0cc271b55e4c663fa5e9956075e528d9d
liberodark <liberodark@gmail.com>
2020-03-02 15:41:47 +0100

    Add files via upload

---
+++ sync-centos-rsyslog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This is a sample mirroring script.
+HOME="/var/www/mirror"
+TARGET="${HOME}/rsyslog"
+TMP="${HOME}/.tmp/rsyslog"
+LOCK="/tmp/rsync-rsyslog.lock"
+
+# NOTE: You'll probably want to change this or remove the --bwlimit setting in
+# the rsync call below
+BWLIMIT=10000
+
+SOURCE="rsync://rpms.adiscon.com/v8-stable/"
+
+[ ! -d "${TARGET}" ] && mkdir -p "${TARGET}"
+[ ! -d "${TMP}" ] && mkdir -p "${TMP}"
+
+exec 9>"${LOCK}"
+flock -n 9 || exit
+
+if ! stty &>/dev/null; then
+    QUIET="-q"
+fi
+
+rsync --exclude 'epel-5' \
+    --exclude 'epel-6' \
+    --exclude 'i386' \
+    --delete-excluded \
+    -rtlvH \
+    --safe-links \
+    --bwlimit=${BWLIMIT} \
+    --delete-after --progress \
+    -h ${QUIET} \
+    --timeout=600 \
+    --contimeout=120 -p \
+    --delay-updates \
+    --no-motd \
+    --temp-dir="${TMP}" \
+    ${SOURCE} \
+    "${TARGET}"
