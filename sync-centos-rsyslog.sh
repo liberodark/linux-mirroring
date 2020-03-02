d2d2de212676abd02ff5ae7261a436a8ac73b54f
liberodark <liberodark@gmail.com>
2020-03-02 15:58:06 +0100

    Update sync-centos-rsyslog.sh

--- sync-centos-rsyslog.sh
+++ sync-centos-rsyslog.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 # This is a sample mirroring script.
-HOME="/var/www/mirror"
+HOME="/tmp/www/mirror"
 TARGET="${HOME}/rsyslog"
 TMP="${HOME}/.tmp/rsyslog"
 LOCK="/tmp/rsync-rsyslog.lock"
 
-# NOTE: You'll probably want to change this or remove the --bwlimit setting in
-# the rsync call below
-BWLIMIT=10000
-
-SOURCE="rsync://rpms.adiscon.com/v8-stable/"
+SOURCE="http://rpms.adiscon.com/v8-stable/"
 
 [ ! -d "${TARGET}" ] && mkdir -p "${TARGET}"
 [ ! -d "${TMP}" ] && mkdir -p "${TMP}"
@@ -21,19 +17,14 @@ if ! stty &>/dev/null; then
     QUIET="-q"
 fi
 
-rsync --exclude 'epel-5' \
-    --exclude 'epel-6' \
-    --exclude 'i386' \
-    --delete-excluded \
-    -rtlvH \
-    --safe-links \
-    --bwlimit=${BWLIMIT} \
-    --delete-after --progress \
-    -h ${QUIET} \
-    --timeout=600 \
-    --contimeout=120 -p \
-    --delay-updates \
-    --no-motd \
-    --temp-dir="${TMP}" \
-    ${SOURCE} \
-    "${TARGET}"
+pushd "${TARGET}" || exit
+wget --mirror \
+    --no-parent \
+    --cut-dirs=1 \
+    --exclude-directories=epel-5 \
+    --exclude-directories=epel-6 \
+    --exclude-directories=i386 \
+    ${SOURCE}
+rm -f rpms.adiscon.com/rsyslog.repo
+rm -rf rpms.adiscon.com/index.html*
+popd || exit
