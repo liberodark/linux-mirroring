fa4f5a55319f7c403c5b453231d6122fd7a41752
liberodark <liberodark@gmail.com>
2021-02-10 11:59:27 +0100

    Update sync-centos-mariadb.sh

--- sync-centos-mariadb.sh
+++ sync-centos-mariadb.sh
@@ -5,7 +5,7 @@ TARGET="${HOME}/mariadb"
 TMP="${HOME}/.tmp/mariadb"
 LOCK="/tmp/wget-mariadb.lock"
 
-SOURCE="http://yum.mariadb.org/10.4/centos7-amd64"
+SOURCE="http://yum.mariadb.org/10.5/centos7-amd64"
 
 [ ! -d "${TARGET}" ] && mkdir -p "${TARGET}"
 [ ! -d "${TMP}" ] && mkdir -p "${TMP}"
@@ -17,22 +17,22 @@ pushd "${TARGET}" || exit
 wget --mirror \
     --no-parent \
     --cut-dirs=1 \
-    --exclude-directories='/10.4/centos6-amd64' \
-    --exclude-directories='/10.4/centos7-aarch64' \
-    --exclude-directories='/10.4/centos7-ppc64' \
-    --exclude-directories='/10.4/centos7-ppc64le' \
-    --exclude-directories='/10.4/centos73-ppc64' \
-    --exclude-directories='/10.4/centos73-ppc64le' \
-    --exclude-directories='/10.4/centos74-aarch64' \
-    --exclude-directories='/10.4/centos8-ppc64le' \
-    --exclude-directories='/10.4/fedora*' \
-    --exclude-directories='/10.4/opensuse*' \
-    --exclude-directories='/10.4/rhel*' \
-    --exclude-directories='/10.4/sles*' \
-    --exclude-directories='/10.4/centos/6*' \
-    --exclude-directories='/10.4/centos/*/aarch64' \
-    --exclude-directories='/10.4/centos/*/ppc64' \
-    --exclude-directories='/10.4/centos/*/ppc64le' \
+    --exclude-directories='/10.5/centos6-amd64' \
+    --exclude-directories='/10.5/centos7-aarch64' \
+    --exclude-directories='/10.5/centos7-ppc64' \
+    --exclude-directories='/10.5/centos7-ppc64le' \
+    --exclude-directories='/10.5/centos73-ppc64' \
+    --exclude-directories='/10.5/centos73-ppc64le' \
+    --exclude-directories='/10.5/centos74-aarch64' \
+    --exclude-directories='/10.5/centos8-ppc64le' \
+    --exclude-directories='/10.5/fedora*' \
+    --exclude-directories='/10.5/opensuse*' \
+    --exclude-directories='/10.5/rhel*' \
+    --exclude-directories='/10.5/sles*' \
+    --exclude-directories='/10.5/centos/6*' \
+    --exclude-directories='/10.5/centos/*/aarch64' \
+    --exclude-directories='/10.5/centos/*/ppc64' \
+    --exclude-directories='/10.5/centos/*/ppc64le' \
     ${SOURCE}
 sudo rm yum.mariadb.org/index.html
 popd || exit
